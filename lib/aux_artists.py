c815556904410069172efe170f3d57747485978e
Jae-Joon Lee <lee.j.joon@gmail.com>
2009-07-15 23:37:05 -0400

    compass size etc. is now calculated during the drawing time

--- lib/aux_artists.py
+++ lib/aux_artists.py
@@ -12,7 +12,7 @@ from pywcsgrid2.wcs_helper import estimate_angle
 
 class AnchoredCompass(AnchoredOffsetbox):
     def __init__(self, ax, transSky2Pix, loc,
-                 arrow_length=0.15,
+                 arrow_fraction=0.15,
                  txt1="E", txt2="N",
                  delta_a1=0, delta_a2=0,
                  pad=0.1, borderpad=0.5, prop=None, frameon=False,
@@ -20,61 +20,80 @@ class AnchoredCompass(AnchoredOffsetbox):
         """
         Draw an arrows pointing the directions of E & N
 
-        arrow_length : length of the arrow as a fraction of axes size
+        arrow_fraction : length of the arrow as a fraction of axes size
 
         pad, borderpad in fraction of the legend font size (or prop)
         """
+
+        self._ax = ax
+        self._transSky2Pix = transSky2Pix
         self._box = AuxTransformBox(ax.transData)
-        #self.ellipse = Ellipse((0,0), width, height, angle)
-
-
-        #wcs_proj = axes.projection
-        x0, y0 = ax.viewLim.x0, ax.viewLim.y0
-        a1, a2 = estimate_angle(transSky2Pix, x0, y0)
-        a1, a2 = a1+delta_a1, a2+delta_a2
-
-        D = min(ax.viewLim.width, ax.viewLim.height)
-        d = D * arrow_length
-        x1, y1 = x0+d*np.cos(a1/180.*np.pi), y0+d*np.sin(a1/180.*np.pi)
-        x2, y2 = x0+d*np.cos(a2/180.*np.pi), y0+d*np.sin(a2/180.*np.pi)
+        self.delta_a1, self.delta_a2 = delta_a1, delta_a2
+        self.arrow_fraction = arrow_fraction
 
         kwargs = dict(mutation_scale=11,
                       shrinkA=0,
                       shrinkB=5)
-        arrow_E = FancyArrowPatch(posA=(x0, y0), posB=(x1, y1),
-                                  arrowstyle="->",
-                                  arrow_transmuter=None,
-                                  connectionstyle="arc3",
-                                  connector=None,
-                                  **kwargs)
-        arrow_N = FancyArrowPatch(posA=(x0, y0), posB=(x2, y2),
-                                  arrowstyle="->",
-                                  arrow_transmuter=None,
-                                  connectionstyle="arc3",
-                                  connector=None,
-                                  **kwargs)
 
-        d2 = d
-        x1t, y1t = x0+d2*np.cos(a1/180.*np.pi), y0+d2*np.sin(a1/180.*np.pi)
-        x2t, y2t = x0+d2*np.cos(a2/180.*np.pi), y0+d2*np.sin(a2/180.*np.pi)
+        self.arrow1 = FancyArrowPatch(posA=(0, 0), posB=(1, 1),
+                                      arrowstyle="->",
+                                      arrow_transmuter=None,
+                                      connectionstyle="arc3",
+                                      connector=None,
+                                      **kwargs)
+        self.arrow2 = FancyArrowPatch(posA=(0, 0), posB=(1, 1),
+                                      arrowstyle="->",
+                                      arrow_transmuter=None,
+                                      connectionstyle="arc3",
+                                      connector=None,
+                                      **kwargs)
 
 
-        txt1 = Text(x1t, y1t, txt1, rotation=a1-180,
-                    rotation_mode="anchor",
-                    va="center", ha="right")
-        txt2 = Text(x2t, y2t, txt2, rotation=a2-90,
-                    rotation_mode="anchor",
-                    va="bottom", ha="center")
+        x1t, y1t, x2t, y2t = 1, 1, 1, 1
+        self.txt1 = Text(x1t, y1t, txt1, rotation=0,
+                         rotation_mode="anchor",
+                         va="center", ha="right")
+        self.txt2 = Text(x2t, y2t, txt2, rotation=0,
+                         rotation_mode="anchor",
+                         va="bottom", ha="center")
 
 
-        self._box.add_artist(arrow_E)
-        self._box.add_artist(arrow_N)
+        self._box.add_artist(self.arrow1)
+        self._box.add_artist(self.arrow2)
 
-        self._box.add_artist(txt1)
-        self._box.add_artist(txt2)
+        self._box.add_artist(self.txt1)
+        self._box.add_artist(self.txt2)
 
         AnchoredOffsetbox.__init__(self, loc, pad=pad, borderpad=borderpad,
                                    child=self._box,
                                    prop=prop,
                                    frameon=frameon)
 
+    def _update_arrow(self, renderer):
+        ax = self._ax
+
+        x0, y0 = ax.viewLim.x0, ax.viewLim.y0
+        a1, a2 = estimate_angle(self._transSky2Pix, x0, y0)
+        a1, a2 = a1+self.delta_a1, a2+self.delta_a2
+
+        D = min(ax.viewLim.width, ax.viewLim.height)
+        d = D * self.arrow_fraction
+        x1, y1 = x0+d*np.cos(a1/180.*np.pi), y0+d*np.sin(a1/180.*np.pi)
+        x2, y2 = x0+d*np.cos(a2/180.*np.pi), y0+d*np.sin(a2/180.*np.pi)
+
+        self.arrow1.set_positions((x0, y0), (x1, y1))
+        self.arrow2.set_positions((x0, y0), (x2, y2))
+
+        d2 = d
+        x1t, y1t = x0+d2*np.cos(a1/180.*np.pi), y0+d2*np.sin(a1/180.*np.pi)
+        x2t, y2t = x0+d2*np.cos(a2/180.*np.pi), y0+d2*np.sin(a2/180.*np.pi)
+
+        self.txt1.set_position((x1t, y1t))
+        self.txt1.set_rotation(a1-180)
+        self.txt2.set_position((x2t, y2t))
+        self.txt2.set_rotation(a2-90)
+
+
+    def draw(self, renderer):
+        self._update_arrow(renderer)
+        super(AnchoredCompass, self).draw(renderer)
