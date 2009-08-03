9db35eafe58d1aa79d969335fef5514d1858f720
Jae-Joon Lee <lee.j.joon@gmail.com>
2009-08-03 19:31:17 -0400

    updated demo_inset.py

--- examples/demo_inset.py
+++ examples/demo_inset.py
@@ -1,22 +1,19 @@
 import matplotlib.pyplot as plt
 import pyfits
 from mpl_toolkits.axes_grid.axes_grid import AxesGrid
-import pywcs
 from pywcsgrid2.axes_wcs import AxesWcs, GridHelperWcs
 from mpl_toolkits.axes_grid.inset_locator import zoomed_inset_axes, mark_inset
 from mpl_toolkits.axes_grid.axislines import Axes
 
 if 1:
 
-    f = pyfits.open("data/E0102_E0300-4000.fits")
+    f = pyfits.open("pspc_skyview.fits")
     d = f[0].data
     h = f[0].header
 
     fig = plt.figure(1)
-    fig.clf()
 
-    wcs = pywcs.WCS(h)
-    grid_helper = GridHelperWcs(wcs)
+    grid_helper = GridHelperWcs(wcs=h)
     grid_helper.update_wcsgrid_params(label_density=(5,5))
 
     grid = AxesGrid(fig, (1, 1, 1), nrows_ncols = (1, 1),
@@ -24,31 +21,30 @@ if 1:
                     cbar_location="right",
                     axes_class=(AxesWcs, dict(grid_helper=grid_helper)))
 
-
-    im = grid[0].imshow(d, origin="lower", cmap=plt.cm.gray_r,
+    main_axes = grid[0]
+    cb_axes = grid.cbar_axes[0] # colorbar axes
+    
+    im = main_axes.imshow(d, origin="lower", cmap=plt.cm.gray_r,
+                        vmin=4.e-05, vmax=0.00018,
                         interpolation="nearest")
 
-    grid.cbar_axes[0].colorbar(im)
-    grid.cbar_axes[0].axis["right"].major_ticklabels.set_visible(True)
+    cb_axes.colorbar(im)
+    cb_axes.axis["right"].major_ticklabels.set_visible(True)
 
-    #grid_helper_inset = GridHelperWcs(wcs)
-    #grid_helper_inset.update_wcsgrid_params(label_density=(3,3))
-
-    axins = zoomed_inset_axes(grid[0], 3, loc=1,
+    axins = zoomed_inset_axes(main_axes, zoom=3, loc=1,
                               axes_class=Axes)
-    #                          axes_kwargs=dict(grid_helper=grid_helper_inset))
 
     for a in axins.axis.values():
-        a.major_ticklabels.set_visible(False)
-        a.major_ticks.set_visible(False)
+        a.toggle(all=False, ticks=True)
 
-    axins.imshow(d, origin="lower", interpolation="nearest",
-                 cmap=plt.cm.gray_r)
+    im2 = axins.imshow(d, origin="lower", interpolation="nearest",
+                       vmin=9.e-05, vmax=18.e-05,
+                       cmap=plt.cm.gray_r)
 
-    axins.set_xlim(23, 30)
-    axins.set_ylim(22, 29)
+    axins.set_xlim(120, 160)
+    axins.set_ylim(120, 160)
 
-    mark_inset(grid[0], axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    mark_inset(main_axes, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
     plt.draw()
     plt.show()
