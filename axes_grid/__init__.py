b49cdac4863283f92de57a8bdab7fb8fe8355acc
Jae-Joon Lee <lee.j.joon@gmail.com>
2010-02-04 10:14:52 -0500

    add some import in __init__.py

--- axes_grid/__init__.py
+++ axes_grid/__init__.py
@@ -1,2 +1,8 @@
 import mpl_toolkits.axes_grid
+
+import mpl_toolkits.axes_grid.axes_size as Size
+from mpl_toolkits.axes_grid.axes_divider import Divider, SubplotDivider, \
+     LocatableAxes, make_axes_locatable
+from mpl_toolkits.axes_grid.axes_grid import Grid, ImageGrid, AxesGrid
+
 __path__ = mpl_toolkits.axes_grid.__path__
