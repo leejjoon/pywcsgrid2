4d6dd5832a232773c65f1f1cb6b0234d1b3244de
Jae-Joon Lee <lee.j.joon@gmail.com>
2010-11-08 01:52:48 +0900

    remove obsolete axes_grid entry in setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from distutils.core import setup
 def main():
 
     setup(name = "pywcsgrid2",
-          version = "0.1a2",
+          version = "0.1b1",
           description = "pywcsgrid2 is a python module to be used with matplotlib for displaying astronomical fits images",
           author = "Jae-Joon Lee",
           author_email = "lee.j.joon@gmail.com",
           url="http://leejjoon.github.com/pywcsgrid2/",
           license = "MIT",
           platforms = ["Linux","Mac OS X"],
-          packages = ['pywcsgrid2','pywcsgrid2.axes_grid'],
+          packages = ['pywcsgrid2'],
           package_dir={'pywcsgrid2':'lib',
-                       'pywcsgrid2.axes_grid':'axes_grid',
                        },
 
           )
