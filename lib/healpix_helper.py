42923355855a53cd7d6df23f666c2c74a07c7068
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-04-16 17:13:32 +0900

    fix healpix_helper to take of nans

--- lib/healpix_helper.py
+++ lib/healpix_helper.py
@@ -17,7 +17,7 @@ class HealpixData(object):
         self._nested = nested
         self._flipy = flipy
         self._coord = coord
-        
+
     def get_projected_map(self, header):
 
         map_shape = (header["naxis2"], header["naxis1"])
@@ -33,8 +33,8 @@ class HealpixData(object):
             if (map_coord is not None) and (map_coord != self._coord):
                 warnings.warn(" doing the conversion " + map_coord)
                 phi, theta = sky2sky(map_coord, self._coord)(phi, theta)
-                
-                
+
+
         if self._flipy:
             theta -= 90
             theta *= -np.pi/180.
@@ -49,30 +49,38 @@ class HealpixData(object):
         else:
             ang2pix = healpy._healpy_pixel_lib._ang2pix_ring
 
-        ipix = ang2pix(self._nside, theta, phi)
 
-        map_data = self._data[ipix].reshape(map_shape)
+        # some values could be NaNs. Maske those out before calling
+        # ang2pix and recover them.
+        mask = np.isfinite(theta) & np.isfinite(theta)
+
+        ipix = ang2pix(self._nside, theta[mask], phi[mask])
+
+        map_data_ = self._data[ipix]
+        map_data = np.empty(map_shape, dtype=map_data_.dtype)
+        map_data.fill(np.nan)
+        map_data.flat[mask] = map_data_
 
         return map_data
 
 
 if __name__ == '__main__':
-    
+
     fname = "LAB_fullvel.fits"
     f = pyfits.open(fname)
     header = f[1].header
-    
+
     ordering = header["ordering"]
     nside = header["nside"]
     data = f[1].data["temperature"]
-    
+
     healpix_data = HealpixData(nside, data.flat, nested=False)
 
     fits_name = "lambda_mollweide_halpha_fwhm06_0512.fits"
     f2 = pyfits.open(fits_name)
 
     d = healpix_data.get_projected_map(f2[1].header)
-    
+
     #data2 = f2[1].data
     #header2 = f2[1].header
 
