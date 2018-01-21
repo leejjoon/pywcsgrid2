531c07ab9f2ae3709926dd9b51fa520a5e52df32
Jae-Joon Lee <lee.j.joon@gmail.com>
2018-01-21 23:15:22 +0900

    use simple setuptools

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from ez_setup import use_setuptools
-use_setuptools()
+# from ez_setup import use_setuptools
+# use_setuptools()
 
+# from setuptools import setup
 from setuptools import setup
-
 
 def main():
 
